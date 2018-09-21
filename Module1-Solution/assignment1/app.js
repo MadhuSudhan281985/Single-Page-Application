184d11e5b91acaeb4ccfc33f39e8749acd701216
MadhuSudhan281985 <43476118+MadhuSudhan281985@users.noreply.github.com>
2018-09-21 20:40:57 +0530

    Assignment 1 Solution

---
+++ Module1-Solution/assignment1/app.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.menuItems = "";
+        
+        $scope.lunchItemCount = function () {
+            var itemsCount = $scope.menuItems;
+            if (itemsCount != "") {
+                var items = itemsCount.split(',');
+                if (items.length <= 3) {
+                    $scope.showmessage = "Enjoy!";
+                    $scope.fontColour = "fontColorGreen";
+                    $scope.borderColour = "borderColorGreen";
+                }
+                else if (items.length > 3) {
+                    $scope.showmessage = "Too much!";
+                    $scope.fontColour = "fontColorGreen";
+                    $scope.borderColour = "borderColorGreen";
+                }
+            }
+            else {
+                $scope.showmessage = "Please enter data first";
+                $scope.fontColour = "fontColorRed";
+                $scope.borderColour = "borderColorRed";
+            }
+        };
+    }    
+})();
+
+
